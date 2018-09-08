eea13829c3593fd2b43ec3921e6e52de37f7515b
Gerardo Saucedo <jegsg@hotmail.es>
2018-09-08 14:37:09 -0500

    Continuacion del Lexico de LYA2

---
+++ Lexico/Output.java
@@ -0,0 +1,21 @@
+package Lexico;
+
+import java.util.*;
+
+public class Output {
+	List lexemas; 
+	
+	public Output() {
+		lexemas = new ArrayList();
+	}
+	
+	
+	
+	public static void main(String [] KrustierDock) {
+		
+	}
+	
+	public void agregarLexema(String lexema) {
+		lexemas.add(lexema);
+	}
+}
---
+++ Lexico/Token.java
@@ -0,0 +1,122 @@
+package Lexico;
+//REGLAS
+import java.util.ArrayList;
+import java.util.List;
+
+public class Token {
+	
+	String tipoOperador, listaString;
+	boolean valor;
+	List lexemas;
+	
+	public Token() {
+		tipoOperador = "";
+		listaString = "";
+		valor = false;
+		lexemas = new ArrayList();
+	}
+
+	public boolean isPalabraReservada(String palabra) {
+		switch(palabra) { 
+		case "PROG": case "VAR": case "PROC": case "INICIO": case "FIN": 
+		case "ENTER": case "REAL": case "STRING": case "LIMPIAR": case "VEXY": 
+		case "LEER": case "ESCRIBIR": case "REPITE": case "HASTA": case "MIENTRAS": 
+		case "SI": case "SINO": case "EJECUTA": case "AND": case "OR": return true;
+		default: return false;
+		}
+	}
+	
+	public boolean isOperador(String operador) {
+		switch(operador) {
+		case "+": case "-": case "*": case "/":
+		case "<": case "<=": case "<>": case ">": case ">=": case "=": 
+		case "&&": case "||": case "!": return true;     
+		default: return false;
+		}
+	}
+	
+	public String getTipoOperador(String operador) {
+		switch(operador) {
+		case "+": case "-": case "*": case "/": tipoOperador = "Aritmetico"; break;
+		case "<": case "<=": case "<>": case ">": case ">=": case "=": tipoOperador = "Relacional"; break;
+		case "&&": case "||": case "!": tipoOperador = "Logico"; break;
+		}
+		return tipoOperador;
+	}
+	
+	public boolean isSpecialCharacter(String character) {
+		switch(character){
+		case ";": case "[": case "]": case ",": case ":": case "(": case ")": case ":=": return true;
+		default: return false;
+		}
+	}
+	
+	public boolean isSpecialCharacterNoToken(String character) {   ////////////REVIEW
+		switch(character){
+		case "\"": case ".": case "\\":return true;//AGREGAR BCO/TAB/EOLN/ EOF
+		default: return false;
+		}
+	}
+	
+	public boolean isDelimitadorToken(char caracter) {
+		//char EOL = System.getProperty("line.separator");
+		switch(caracter) {
+		case '+': case '-': case '*': case '/': case '<': case '=': 
+		case '>': case '&': case '|': case '!': case ';': case '[': 
+		case ']': case ',': case ':': case '(': case ')':  
+		return true;
+		default: return false;
+		}
+	}
+	
+	public boolean isDelimitadorNoToken(char caracter) {
+		switch(caracter) {
+		case ' ': case '\t': return true;
+		default: return false;
+		}
+	}
+	
+	public boolean isIdentificador(String palabra) {
+		if(palabra.matches("^[a-zA-Z].*") && palabra.length()<=6) { //VERIFICAR QUE INICIA CON LETRA y TENGA MENOS o igual a 6 CARACTERES
+			 valor = true;
+		}
+		else valor = false;
+		return valor;
+	}
+	
+	public boolean isNumeroEntero(String constante){
+		if(constante.matches("\\d+")) return true; //EXPRESON REGULAR SOLO ACEPTA NUMEROS Equivalent to [0-9].
+		else return false;
+	}
+	
+	public boolean isNumeroReal(String numero) {
+		if(numero.matches(("^\\d+\\.\\d+$"))) return true;
+		else return false;	
+	}
+	
+	public boolean isString(String cadena) {
+		if(cadena.charAt(0) == '\"' && cadena.charAt(cadena.length()-1) == '\"') valor = true;
+		else valor = false;
+		return valor;
+	}
+	
+	public boolean isComentario(String cadena) {
+		if(cadena.length()>2) {
+			if(cadena.charAt(0) == '\\' && cadena.charAt(1) == '\\') valor = true;
+			else valor = false;
+		}else valor=false;
+		
+		return valor;
+	}
+	
+	public String imprimirLista(List lista) {
+		listaString = "";
+		for (Object element: lista) {
+           listaString = listaString + element;
+        }
+		return listaString;	
+	}
+	
+	
+	
+}//END CLASS
